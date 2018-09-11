c4383c25e4d61961fb5d856ee47608d19c309fe9
vasilegrafu <vasilegrafu@gmail.com>
2018-09-11 11:20:59 +0100

    The solution will open with VSCode-win32-x64-1.27.1

--- solution/DevFX.bat
+++ solution/DevFX.bat
@@ -2,4 +2,4 @@
 
 SET PATH=i:\Installations\dotnet-sdk-2.1.401-win-x64;%PATH%
 
-i:\Installations\VSCode-win32-x64-1.26.1\Code.exe DevFX.code-workspace
+i:\Installations\VSCode-win32-x64-1.27.1\Code.exe DevFX.code-workspace
