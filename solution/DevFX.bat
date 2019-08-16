ff24056f5eeb9e55f94ea22c188cfcaa60594330
vasilegrafu <vasilegrafu@gmail.com>
2019-08-16 14:21:31 +0300

    Switched to VSCode-win32-x64-1.37.1

--- solution/DevFX.bat
+++ solution/DevFX.bat
@@ -1,5 +1,5 @@
 @echo off
 
-SET PATH=i:\Installations\dotnet-sdk-2.1.401-win-x64;%PATH%
+SET PATH=i:\Installations\dotnet-sdk-2.2.401-win-x64;%PATH%
 
-i:\Installations\VSCode-win32-x64-1.28.2\Code.exe DevFX.code-workspace
+i:\Installations\VSCode-win32-x64-1.37.1\Code.exe DevFX.code-workspace
