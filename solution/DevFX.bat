cf62881696f1db68ef4e800a59bc8c0656c56cbc
vasilegrafu <vasilegrafu@gmail.com>
2020-07-19 23:02:44 +0300

    Switch to .Net 3.1

--- solution/DevFX.bat
+++ solution/DevFX.bat
@@ -2,4 +2,4 @@
 
 SET PATH=i:\Installations\dotnet-sdk-2.2.401-win-x64;%PATH%
 
-i:\Installations\VSCode-win32-x64-1.37.1\Code.exe DevFX.code-workspace
+i:\Installations\VSCode-win32-x64-1.46.1\Code.exe DevFX.code-workspace
