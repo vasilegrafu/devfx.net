8eadb8fe1d34f0dd304dfd7e2e1cd319c4229678
vasilegrafu <vasilegrafu@gmail.com>
2022-10-11 21:35:27 +0300

    .

--- solution/DevFX.bat
+++ solution/DevFX.bat
@@ -2,4 +2,4 @@
 
 SET PATH=i:\Installations\dotnet-sdk-3.1.301-win-x64;%PATH%
 
-i:\Installations\VSCode-win32-x64-1.46.1\Code.exe DevFX.code-workspace
+i:\Installations\VSCode-win32-x64-1.72.1\Code.exe DevFX.code-workspace
